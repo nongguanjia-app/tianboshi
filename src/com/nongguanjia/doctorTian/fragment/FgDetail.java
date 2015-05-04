19ea58d58dc0744159e6a53bed4288728940a432
tx <549682063@qq.com>
2015-05-04 15:52:52 +0800

    解决课程详情bug

--- src/com/nongguanjia/doctorTian/fragment/FgDetail.java
+++ src/com/nongguanjia/doctorTian/fragment/FgDetail.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -148,6 +149,17 @@ public class FgDetail extends Fragment {
 				return true;
 			}
 		});
+		
+		expListView.setOnGroupClickListener(new OnGroupClickListener() {
+			
+			@Override
+			public boolean onGroupClick(ExpandableListView arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				return true;
+			}
+		});
+		
 	}
 	
 }
