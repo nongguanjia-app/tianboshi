4112a8f202c7639cf31d20480d4487a4fa5e8cdd
Yue6 <yuechunsheng@126.com>
2015-05-06 17:33:52 +0800

    修改标题字显示不全

--- src/com/nongguanjia/doctorTian/view/PagerSlidingTabStrip.java
+++ src/com/nongguanjia/doctorTian/view/PagerSlidingTabStrip.java
@@ -96,10 +96,10 @@ public class PagerSlidingTabStrip extends HorizontalScrollView {
 	private int indicatorHeight = 8;
 	private int underlineHeight = 2;
 	private int dividerPadding = 12;
-	private int tabPadding = 24;
+	private int tabPadding = 10;
 	private int dividerWidth = 1;
 
-	private int tabTextSize = 12;
+	private int tabTextSize = 13;
 	private int tabTextColor = 0xFF666666;
 	private int selectedTabTextColor = 0xFF666666;
 	private Typeface tabTypeface = null;
